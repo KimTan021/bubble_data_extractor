e629811b6711a0f9ead419a6adffc3a1d3a474af
KimTan021 <tankimemerson05@gmail.com>
2024-08-07 15:49:04 +0800

    modify about.py

--- views/about.py
+++ views/about.py
@@ -3,9 +3,6 @@ import streamlit as st
 
 def app():
     st.markdown("# About the App 📝")
-    st.markdown("**App Name:** Bubble Data Extractor")
-    st.markdown(
-        "**Developer:** Kim Emerson M. Tan (Backend Developer Intern PITX)")
     
     st.markdown("""
         <p style="text-align: justify;">The Bubble Data Extractor is a sophisticated tool designed for seamless integration with Bubble's application database. This application, developed using Streamlit and Python, enables users to fetch and export data efficiently by constructing dynamic API URL endpoints based on user-specified constraints. By leveraging this tool, users can tailor their data retrieval processes to meet specific criteria, ensuring that they receive precisely the data they need for their analysis, reporting, or other data-driven tasks.</p>
