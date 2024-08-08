1599274a0fc10181e6fc5d52c151bbd584c25e24
KimTan021 <tankimemerson05@gmail.com>
2024-08-08 09:48:44 +0800

    add docstring

--- create_excel.py
+++ create_excel.py
@@ -2,6 +2,15 @@ import streamlit as st
 from data_manager import convert_to_excel
 
 def create_excel(data):
+    """
+    Converts the given data into an Excel file and provides a download button to download the file.
+
+    Args:
+        data: The data to be converted into an Excel file.
+
+    Returns:
+        None
+    """
     excel_file = convert_to_excel(data)
     st.download_button(label="Download Excel",
                     data=excel_file,
