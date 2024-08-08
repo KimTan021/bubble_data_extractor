78684b371fbe3476ee749a46b5f113559e6a2b02
KimTan021 <tankimemerson05@gmail.com>
2024-08-08 09:49:56 +0800

    add docstring to function

--- session_state_manager.py
+++ session_state_manager.py
@@ -1,6 +1,20 @@
 import streamlit as st
 
 def manage_states():
+    """
+    Manages the session states for the application.
+
+    This function checks if the required session states are present and initializes them if not.
+
+    Session States:
+    - current_page: The current page number.
+    - filters: A list of filters applied.
+    - prev_endpoint: The previous endpoint.
+    - filtering_done: Indicates if filtering has been done.
+
+    Returns:
+    None
+    """
     if 'current_page' not in st.session_state:
         st.session_state.current_page = 0
 
