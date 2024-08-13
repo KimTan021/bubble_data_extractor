c99771de5b9564f1899e20fb0598114861968247
KimTan021 <tankimemerson05@gmail.com>
2024-08-13 15:14:48 +0800

    fix error in packages

--- views/chatbot.py
+++ views/chatbot.py
@@ -4,12 +4,9 @@ from llama_index.llms.groq import Groq
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 import streamlit as st
 from streamlit_chat import message
-import requests
-from bs4 import BeautifulSoup
 
-def app():
-    @st.cache_data
-    def get_response_from_chatbot(user_query):
+@st.cache_resource
+def load_model():
         llm = Groq(model="llama3-70b-8192", api_key=st.secrets["API_KEY"])
         embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
         Settings.llm = llm
@@ -17,13 +14,20 @@ def app():
         documents = SimpleDirectoryReader(input_files=['README.md'], ).load_data()
         index = VectorStoreIndex.from_documents(documents)
         query_engine = index.as_query_engine()
+        
+        return query_engine
+
+def app():
+    @st.cache_data
+    def get_response_from_chatbot(user_query):
+        
+        query_engine = load_model()
 
         # Get response from the model
         response = query_engine.query(user_query)
         
         return response.response, response.source_nodes
 
-    # Streamlit app
     st.title("🤖 Hi There!")
 
     # Input text box for user query
