6a63e2322c66845ef95ed599ee937fbe592e6c33
KimTan021 <tankimemerson05@gmail.com>
2024-08-13 16:47:34 +0800

    modify limitations

--- views/limitations.py
+++ views/limitations.py
@@ -10,15 +10,20 @@ def app():
 
     st.markdown("##### Limitations")
     st.markdown("""
-        <p style="text-align: justify;">
-        - There is a limit of 50,000 items in any GET request, meaning that if a database contains 100,000 items and the cursor is set at 50,001 no results will be returned.
-        </p>
-        <p style="text-align: justify;">
-        - For the Enterprise plan the limit is 10,000,000 items.
-        </p>
-        <p style="text-align: justify;">
-        - The URL endpoint you will use in the app to extract data is limited to Bubble's Data API endpoints. The main reason for this limitation is that different API calls have their own methods of sending data, specifically the JSON data they transmit. As a result, the code is designed to accept data formatted according to how Bubble sends their data using their API.
-        </p>
+        <ul>
+            <li style="text-align: justify;">
+                You can only get 100 rows of data per API call.
+            </li>
+            <li style="text-align: justify;">
+                There is a limit of 50,000 items in any GET request, meaning that if a database contains 100,000 items and the cursor is set at 50,001 no results will be returned.
+            </li>
+            <li style="text-align: justify;">
+                For the Enterprise plan the limit is 10,000,000 items.
+            </li>
+            <li style="text-align: justify;">
+                The URL endpoint you will use in the app to extract data is limited to Bubble's Data API endpoints. The main reason for this limitation is that different API calls have their own methods of sending data, specifically the JSON data they transmit. As a result, the code is designed to accept data formatted according to how Bubble sends their data using their API.
+            </li>
+        </ul>
     """, unsafe_allow_html=True)
     
     st.markdown("For more information on limitations, please refer to the [Bubble API documentation](https://bubble.io/reference#API.get_api_data).") 
